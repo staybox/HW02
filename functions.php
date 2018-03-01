184c4bf1e8794048d27a03d46c72fd8963c7217d
Andrey <staybox@gmail.com>
2018-03-01 15:53:23 +0300

    Второе задание (изменения в работе)

--- functions.php
+++ functions.php
@@ -18,5 +18,50 @@ function task1($data, $true = false)
     return null;
 }
 
+function task2 ($num,$operation=null) {
 
+    switch ($operation){
+
+        case "+":
+            $s=$num[0];
+            unset($num[0]);
+            foreach($num as $nums){
+                echo $s += $nums;
+            }
+            break;
+        case "-":
+            $s=$num[0];
+            unset($num[0]);
+            foreach($num as $nums){
+                echo $s -= $nums;
+            }
+            break;
+        case "*":
+            $s=$num[0];
+            unset($num[0]);
+            foreach($num as $nums){
+                echo $s *= $nums;
+            }
+            break;
+        case "/":
+            $s=$num[0];
+            unset($num[0]);
+            foreach($num as $nums){
+                if($nums == 0){
+                    echo "В массиве есть значение $nums. Деление на $nums запрещено.";
+                    break;
+                }else {
+                    echo $s /= $nums;
+                    echo "\n";
+                }
+            }
+            break;
+        case null:
+            echo "Не введен второй аргумент. Допустимые аргменты: +,-,*,/.";
+            break;
+        default:
+            echo "Неправильный второй аргумент, можно использовать только: +,-,*,/. Попробуйте изменить значение аргумента и попробовать снова.";
+    }
+
+}
 
